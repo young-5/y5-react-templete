407bd81f07d684a12b0a8633592405aba8e2e213
我只是是码偷 <yangw5@163.com>
2023-12-13 13:21:15 +0800

    perf: 代码优化

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,3 @@
 export { default as CustomScroll } from './CustomScroll'
 export { default as ErrorBoundary } from './ErrorBoundary'
-export { default as Img } from './Img'
-export { default as LazyList } from './LazyList'
-export { default as PicList } from './PicList'
-export { default as HPlic } from './PicList/HPlic'
-export { default as VirtualizedList } from './VirtualizedList'
 
--- src/pages/test/ui/index.tsx
+++ src/pages/test/ui/index.tsx
@@ -1,35 +1,13 @@
-import { CustomScroll, VirtualizedList } from '@/components'
+import { CustomScroll } from '@/components'
 import { Tabs } from 'antd'
 import * as React from 'react'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import CssCP from './CssCP'
-import ImgTs from './ImgTs'
 import cs from './index.module.less'
 const { TabPane } = Tabs
 const Test: React.FC = () => {
-  const imgBoxRef = useRef<any>([])
-  const [updataData, setUpadtaData] = useState(false)
-  useEffect(() => {
-    console.log('imgBoxRef', imgBoxRef.current)
-  }, [imgBoxRef.current])
   const [activeTab, setActiveTab] = useState('1')
-  let data = new Array(1000).fill({})?.map((v: any, index: number) => ({
-    name: `-----------------${index}`,
-    id: index,
-  }))
-  const onUpdata = () => {
-    setUpadtaData((v) => !v)
-  }
-  const columns = React.useCallback(() => {
-    return [
-      {
-        title: '角色名称',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-      },
-    ]
-  }, [])
+
   return (
     <div className={cs.test}>
       <div className={cs.test_title}>ui测试</div>
@@ -54,14 +32,6 @@ const Test: React.FC = () => {
               <div>底部</div>
             </div>
           </CustomScroll>
-        </TabPane>
-
-        <TabPane tab={'虚拟列表'} key={'5'}>
-          <VirtualizedList list={data} />
-        </TabPane>
-
-        <TabPane tab={'图片相关组件'} key={'7'}>
-          <ImgTs />
         </TabPane>
 
         <TabPane tab={'css 知识实践'} key={'8'}>
